14a1880a40300fa03800240c65699cc0c5e3bb5b
ntalydevs <ntalydevs@gmail.com>
2026-03-29 17:12:10 +0300

    feat: routing , tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
@@ -24,6 +28,6 @@ export default function Index() {
         className="mt-4 rounded bg-primary text-white p-4">
         Spotify Subscription
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
